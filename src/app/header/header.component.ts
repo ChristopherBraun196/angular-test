2bc73cd657f6f7f7816a858d485983efb265536f
ChristopherBraun196 <232182424+ChristopherBraun196@users.noreply.github.com>
2026-05-20 17:54:28 +0200

    add first component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-header',
+   templateUrl: 'header.component.html',
+  styleUrl: 'header.component.scss',
+
+})
+export class Header {
+
+}
+
+// @Component({
+//   selector: 'profile-photo',
+//   templateUrl: 'profile-photo.html',
+//   styleUrl: 'profile-photo.css',
+// })
+// export class ProfilePhoto {}
